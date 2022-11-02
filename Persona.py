11119a7cee14ccd291c321e7ae8a49eaa1267138
Yenssy <112731269+yenssyleon@users.noreply.github.com>
2022-11-02 17:32:54 -0500

    Add files via upload

---
+++ Persona.py
@@ -0,0 +1,42 @@
+
+class Persona:
+    def __init__(self):
+        self.nombre=""
+        self.edad=int()
+        self.genero=""
+        self.volumen=""
+        self.bigote=""
+
+    def gritar():
+        pass
+    #aquí gritan :)
+
+    def bailar():
+        pass
+    #aquí bailan :)
+
+    def saltar():
+        pass
+    #aquí saltan
+
+
+sujeto_a= Persona()
+sujeto_a.nombre="Camilo"
+sujeto_a.edad=20
+sujeto_a.genero="Pansexual"
+sujeto_a.volumen="Alto"
+sujeto_a.bigote="FALSE"
+
+sujeto_b= Persona()
+sujeto_a.nombre="Cristian"
+sujeto_a.edad=56
+sujeto_a.genero="Masculino"
+sujeto_a.volumen="Medio"
+sujeto_a.bigote="FALSE"
+
+sujeto_c= Persona()
+sujeto_a.nombre="Lina"
+sujeto_a.edad=19
+sujeto_a.genero="Femenin"
+sujeto_a.volumen="Alto"
+sujeto_a.bigote="FALSE"  
